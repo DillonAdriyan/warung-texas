f638aee2f7da6f4545b3aacc38afc220cc637d68
DillonAdriyan <dillon.adrian00@gmail.com>
2024-10-27 20:54:30 +0700

    push to github

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./*.{html,js}'],
+  theme: {
+    extend: {
+     colors: {
+      'primary': '#3B1E54'
+     }
+    },
+  },
+  plugins: [],
+}
+
